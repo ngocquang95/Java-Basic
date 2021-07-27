bd0695a61833ab98a21c2b926ce6c5558d44a145
Ngoc Quang <ngocquang12395@gmail.com>
2021-07-27 23:33:22 +0700

    Bài tập về vòng lặp

--- java-basic/bai-tap-vong-lap/src/controller/TestMain.java
+++ java-basic/bai-tap-vong-lap/src/controller/TestMain.java
@@ -1,7 +1,7 @@
 package controller;
 
-public class HelloWorld {
+public class TestMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("oke");
     }
 }
